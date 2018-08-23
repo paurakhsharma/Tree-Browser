4b354336093fc9cc6ece078bd90bb878914a8ef2
Paurakh Sharma Humagain <paurakh011@gmail.com>
2018-08-24 00:39:09 +0545

    Add click event for expanding tree

--- src/components/TreeBrowser.vue
+++ src/components/TreeBrowser.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
         <div
+            @click="expanded = !expanded"
             :style="{'margin-left': `${depth*20}px`}"
-            class="node">
-            <h1>{{node.name}}</h1>
+            class="node"
+        >
+            <span 
+                v-if="hasChildren"
+                class="type">{{ expanded ? '&#9660;' : '&#9658;'}}
+            </span>
+            <span v-else>&#9671;</span>
+            {{node.name}}
         </div>
         <TreeBrowser 
+          v-if="expanded"
           v-for="child in node.children"
           :key="child.name"
           :node="child"
@@ -23,13 +31,23 @@ export default {
             type: Number,
             default: 0,
         }
-    }
+    },
+    data() {
+        return {
+            expanded: false,
+        }
+    },
+    computed: {
+        hasChildren() {
+            return this.node.children;
+        }
+    } 
 }
 </script>
 
 <style scoped>
     .node {
         text-align: left;
-
+        font-size: 18px;
     }
 </style>
